711a8eeb3d9b0a21f134f15d66c622b1fec82c86
Víctor Fuertes Fuertes <victor@fuert.es>
2024-02-18 18:13:30 +0100

    Create .exports.sh

---
+++ .zash/.exports.sh
@@ -0,0 +1,72 @@
+# Ver las computadas con el comando 'set'.
+
+setopt auto_cd
+setopt autocd
+setopt nobeep
+
+# Config
+
+export USER_NAME="Víctor Fuertes"
+
+# export TMPDIR=""
+
+export WORDCHARS='*?[]~&;!$%^<>'
+
+export EDITOR="subl"
+export VISUAL="subl"
+
+export LANG="es_ES.UTF-8"
+export LANGUAGE="es_ES.UTF-8"
+export LC_ALL="en_US.UTF-8"
+# export LC_TYPE="es_ES.UTF-8"
+# export LC_CTYPE="es_ES.UTF-8"
+# export LC_MESSAGES="es_ES.UTF-8"
+# export LC_PAPER="es_ES.UTF-8"
+# export LC_NAME="es_ES.UTF-8"
+# export LC_ADDRESS="es_ES.UTF-8"
+# export LC_TELEPHONE="es_ES.UTF-8"
+# export LC_MEASUREMENT="es_ES.UTF-8"
+# export LC_IDENTIFICATION="es_ES.UTF-8"
+# export LC_MONETARY="es_ES.UTF-8"
+# export LC_NUMERIC="es_ES.UTF-8"
+# export LC_TIME="es_ES.UTF-8"
+export LC_COLLATE=C
+export LC_ALL=
+
+# GITHUB_KEY="Key"
+# GITHUB_SECRET="Secret"
+
+export GIT_AUTHOR_EMAIL='victor@fuert.es'
+export GIT_AUTHOR_NAME='Victor Fuertes'
+export GIT_COMMITTER_EMAIL='victor@fuert.es'
+export GIT_COMMITTER_NAME='Victor Fuertes'
+
+# Env
+
+export TZ="Europe/Madrid"
+export HIST_STAMPS="dd.mm.yyyy"
+
+export PAPERSIZE="a4"
+
+export PAGER='less'
+export MANPAGER="less -X"
+export TERM="xterm"
+export TERM_PROGRAM="iTerm2.app"
+export CLICOLOR="1"
+export GREP_OPTIONS="--color=auto"
+
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
+export PATH="/usr/local/bin$PATH"
+export PATH="/usr/local/sbin:$PATH"
+
+export TERM="screen-256color"
+export CLICOLOR="1"
+
+# export TERMINAL_WIDTH=$(tput cols)
+# export TERMINAL_HEIGHT=$(tput lines)
+export SCRIPTS="~/scripts/"
+export NOTES="~/notas/"
+export LOGS="~/logs/"
+export IRC_LOGS="$LOGS/.irc"
+export SCREENSHOTS="~/Desktop/screen"
